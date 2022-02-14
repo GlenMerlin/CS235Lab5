4ea48d63c9b3b33da31ff13f121f8c91d7eee7f0
GlenMerlin <Glenmerlin@protonmail.com>
2022-02-13 20:25:09 -0700

    out_file1.txt working

--- QS.cpp
+++ QS.cpp
@@ -2,39 +2,68 @@
 
 using namespace std;
 
-    QS::QS(){
+QS::QS(){}
+QS::~QS(){}
 
+bool QS::createArray(int capacity){
+    cout << "inside the createArray function" << endl;
+    if (capacity > 0){
+        int valuearray[capacity];
+        return true;
     }
-    QS::~QS(){
+    else{
+        return false;
+    }
+}
 
+bool QS::addToArray(int value){
+    arraySize();
+    if (addIndex < size){
+        cout << "addIndex: " << addIndex << " is less than " << size << "." << endl;
+        valueArray[addIndex] = value;
+        addIndex++;
+        return true;
     }
+    else{
+        return false;
+    }
+
+}
 
 void QS::sortAll(){
-    cout << "inside the function" << endl;
+    // cout << "inside the function" << endl;
 }
 int QS::medianOfThree(int left, int right){
-    cout << "inside the function" << endl;
+    // cout << "inside the function" << endl;
     return 0;
 }
 int QS::partition(int left, int right, int pivotIndex){
-    cout << "inside the function" << endl;
+    // cout << "inside the function" << endl;
     return 0;
 }
 string QS::getArray() const {
-    cout << "inside the function" << endl;
+    string result = "";
+    for (int i = 0; i < addIndex; i++){
+        if (i < addIndex-1){
+            // cout << valueArray[i] << ", " << endl;
+            result += to_string(valueArray[i]) + ",";
+        }
+        else {
+            // cout << "last one: " << valueArray[i] << endl;
+            result += to_string(valueArray[i]) + "";
+        }
+    }
+    return result;
 }
 int QS::getSize() const {
-    cout << "inside the function" << endl;
-    return 0;
-}
-bool QS::addToArray(int value){
-    cout << "inside the function" << endl;
-    return false;
-}
-bool QS::createArray(int capacity){
-    cout << "inside the function" << endl;
-    return false;
+    return addIndex;
 }
 void QS::clear(){
-    cout << "inside the function" << endl;
+    addIndex = 0;
+    size = 0;
+}
+
+void QS::arraySize(){
+    size = sizeof(valueArray)/sizeof(valueArray[0]);
+    // cout << "the size of the array is: " << size << endl;
 }
--- QS.h
+++ QS.h
@@ -1,19 +1,36 @@
+#pragma once
 #include "QSInterface.h"
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 class QS: public QSInterface {
     protected:
-        int valuearray[5];
+        int valueArray[5];
     public:
+    
     QS();
-    ~QS()override; 
+    
+    ~QS();
+    int addIndex = 0;
+    int size;
+
     void sortAll() override;
+    
     int medianOfThree(int left, int right) override;
+    
     int partition(int left, int right, int pivotIndex) override;
+    
     string getArray() const override;
+    
     int getSize() const override;
+    
     bool addToArray(int value) override;
+    
     bool createArray(int capacity) override;
+    
     void clear() override;
+
+    void arraySize();
 };
